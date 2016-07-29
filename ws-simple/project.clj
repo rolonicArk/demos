171e31ea138eccc9fc339ec317e296e8cba191fd
Bill La Forge <laforge49@gmail.com>
2016-07-29 12:40:26 -0400

    updated

---
+++ ws-simple/project.clj
@@ -0,0 +1,18 @@
+(defproject
+  boot-project
+  "0.0.0-SNAPSHOT"
+  :dependencies
+  [[adzerk/boot-cljs "1.7.228-1"]
+   [adzerk/boot-cljs-repl "0.3.3"]
+   [adzerk/boot-reload "0.4.12"]
+   [compojure "1.6.0-beta1"]
+   [hoplon/boot-hoplon "0.2.2"]
+   [hoplon/castra "3.0.0-alpha4"]
+   [hoplon "6.0.0-alpha16"]
+   [org.clojure/clojure "1.9.0-alpha10"]
+   [org.clojure/clojurescript "1.9.93"]
+   [pandeiro/boot-http "0.7.3"]
+   [ring "1.6.0-beta4"]
+   [ring/ring-defaults "0.2.1"]]
+  :source-paths
+  ["src" "assets"])
