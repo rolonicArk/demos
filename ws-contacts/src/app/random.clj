ee70255b309cd1c4c3325017f4d336ae6045c003
Bill La Forge <laforge49@gmail.com>
2016-08-14 13:50:11 -0400

    deleted

--- ws-contacts/src/app/random.clj
+++ ws-contacts/src/app/random.clj
@@ -14,3 +14,14 @@
 (defmethod handler/event-msg-handler :contacts/get-contacts
   [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
   (handler/chsk-send! (:client-id ev-msg) [(:resp-id ?data) @my-contacts]))
+
+(defn broadcast [resp-id data]
+  (let [uids (:any @handler/connected-uids)]
+    (doseq [uid uids]
+      (handler/chsk-send! uid [resp-id data]))))
+
+(defmethod handler/event-msg-handler :contacts/delete
+  [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
+  (let [contact (:contact ?data)]
+    (swap! my-contacts disj contact)
+    (broadcast :contacts/deleted contact)))
