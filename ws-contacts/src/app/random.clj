a290fa544a780f93b427e8555e9f56424e216a93
Bill La Forge <laforge49@gmail.com>
2016-08-14 11:57:24 -0400

    :contacts/initialize

--- ws-contacts/src/app/random.clj
+++ ws-contacts/src/app/random.clj
@@ -3,6 +3,14 @@
 
 (def app handler/routes)
 
-(defmethod handler/event-msg-handler :simple/get-random
+(defonce my-contacts
+  (atom #{{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+          {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+          {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+          {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+          {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+          {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}}))
+
+(defmethod handler/event-msg-handler :contacts/get-contacts
   [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
-  (handler/chsk-send! (:client-id ev-msg) [(:resp-id ?data) (rand-int 100)]))
+  (handler/chsk-send! (:client-id ev-msg) [(:resp-id ?data) @my-contacts]))
